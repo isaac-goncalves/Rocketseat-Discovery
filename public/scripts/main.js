8b884b6c4fe3b55cf4b6bc5415da12bed4e9d096
isaac-goncalves <isaac.correia.2406@gmail.com>
2021-12-22 16:18:11 -0300

    Imprementação da modal

---
+++ public/scripts/main.js
@@ -0,0 +1,15 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+
+const checkButtons = document.querySelectorAll(".actions a")
+console.log("botao clicado ")
+
+
+checkButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        console.log("botao clicado ")
+
+        modal.open()
+    })
+})
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,4 +1,4 @@
-module.exports = function Modal() {
+export default function Modal() {
     function open() {
         document.querySelector('.modal-wrapper').classList.add('active')
     }
