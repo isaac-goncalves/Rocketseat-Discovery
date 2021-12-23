02bd5bb91bd63a481c8ae228ac16b7cdd39551f3
isaac-goncalves <isaac.correia.2406@gmail.com>
2021-12-23 11:44:30 -0300

    modal configurations
    s

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,18 +2,29 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
-const checkButtons = document.querySelectorAll(".actions a")
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
 
-console.log("botao clicado ")
 
-
-checkButtons.forEach(button => {
-    button.addEventListener("click", event => {
-        console.log("botao clicado ")
-
-        modal.open()
-    })
-})
-
+const checkButtons = document.querySelectorAll(".actions a.check")
 
 const deleteButton = document.querySelectorAll(".actions a.delete")
+
+checkButtons.forEach(button => {
+    button.addEventListener("click", handleClick)
+})
+
+// quando o botao delete for clicado ele abre a modal
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
+})
+
+function handleClick(event, check = true) {
+    const text = check ? "Marcar como lida" : "Excluir"
+
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDescription.innerHTML = check ? "Tem certeza que deseja marcar como lida esta pergunta" : "Tem certeza que deseja excluir esta pergunta"
+    modal.open()
+}
