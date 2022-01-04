7b1027fd7401467c61a8ab338b6751665c89e9f7
isaac-goncalves <isaac.correia.2406@gmail.com>
2022-01-04 17:35:11 -0300

    Database Configuration

--- src/db/config.js
+++ src/db/config.js
@@ -1,7 +1,9 @@
-const sqlite3 = require("sqlite3")
-const { open } = require("sqlite")
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
 
 module.exports = () =>
     open() {
-        filename: '.src/'
-    }
+        filename: '.src/db/rocketq.sqlite'
+        driver: sqlite3.Database,
+    });
+[]
