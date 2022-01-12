5ca9ac336391272681522c8fe48742517d3ef9c5
isaac-goncalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-01-12 14:35:06 -0300

    Go

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,8 +1,38 @@
+const Database = require("../db/config");
+
+
 module.exports = {
-    create(req, res) {
+  async create(req, res) {
+    const db = await Database();
+    const pass = req.body.password
 
-        var roomId = 123456
+    let roomId;
 
-        res.redirect(`/room/${roomId}`)
+    for (var i = 0; i < 6; i++) {
+      i == 0
+        ? roomId = Math.floor(Math.random() * 10).toString()
+        : roomId += Math.floor(Math.random() * 10).toString();
     }
-}
+
+    console.log(parseInt(roomId))
+
+// veridicar se o numero ja existe
+
+
+const roomsExistIds  =   await db.all(`SELECT id FROM rooms`)
+
+roomsExistIds.map(roomsExistIds)
+
+    // insere a sala no banco
+
+    await db.run(`INSERT INTO rooms (
+        id,
+        pass
+    )VALUES(
+        ${parseInt(roomId)},
+        "${pass}"
+    )`)
+
+    res.redirect(`/room/${roomId}`);
+  }
+};
