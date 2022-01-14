90e0620a8b561ca18fa5678d8447f989492775e9
isaac-goncalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-01-13 21:02:12 -0300

    go

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,38 +1,42 @@
 const Database = require("../db/config");
 
-
 module.exports = {
   async create(req, res) {
     const db = await Database();
-    const pass = req.body.password
-
+    const pass = req.body.password;
     let roomId;
+    let isRoom = true;
 
-    for (var i = 0; i < 6; i++) {
-      i == 0
-        ? roomId = Math.floor(Math.random() * 10).toString()
-        : roomId += Math.floor(Math.random() * 10).toString();
+    while (isRoom) {
+      // gera um numero aleatorio
+      for (var i = 0; i < 6; i++) {
+        i == 0
+          ? (roomId = Math.floor(Math.random() * 10).toString())
+          : (roomId += Math.floor(Math.random() * 10).toString());
+      }
+
+      console.log(parseInt(roomId));
+
+      // veridicar se o numero ja existe
+
+    await db.close
+    const roomsExistIds = await db.all(`SELECT id FROM rooms`);
+      isRoom = roomsExistIds.some(roomsExistIds => roomsExistIds === roomId);
+
+      // insere a sala no banco se o numero for diferente
+      if (!isRoom) {
+        await db.run(`INSERT INTO rooms (
+              id,
+              pass
+          )VALUES(
+              ${parseInt(roomId)},
+              "${pass}"
+          )`);
+      }
     }
 
-    console.log(parseInt(roomId))
-
-// veridicar se o numero ja existe
-
-
-const roomsExistIds  =   await db.all(`SELECT id FROM rooms`)
-
-roomsExistIds.map(roomsExistIds)
-
-    // insere a sala no banco
-
-    await db.run(`INSERT INTO rooms (
-        id,
-        pass
-    )VALUES(
-        ${parseInt(roomId)},
-        "${pass}"
-    )`)
+    await db.close()
 
     res.redirect(`/room/${roomId}`);
-  }
+  },
 };
